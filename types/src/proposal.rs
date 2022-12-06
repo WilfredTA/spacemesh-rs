10ed8b19070091dc704e21e52e9f7d8ab219b8eb
tannr <tannr@hey.com>
2022-12-06 04:31:59 -0800

    add proposal skeleton

---
+++ types/src/proposal.rs
@@ -0,0 +1,61 @@
+use parity_scale_codec::{Decode, Encode};
+
+use crate::{hashes::{Hash20, hash_256, Hash32}, bytes::Bytes, ballot::Ballot, TxId};
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum ProposalError {
+    #[error("Proposal already initialized")]
+    ProposalAlreadyInit
+}
+
+pub type ProposalResult<T> = Result<T, ProposalError>;
+
+#[derive(Default, Clone, Encode, Decode)]
+pub struct ProposalId(Hash20);
+
+impl From<ProposalId> for Bytes {
+    fn from(id: ProposalId) -> Self {
+        todo!()
+    }
+}
+
+impl ProposalId {
+    pub fn as_hash_32(&self) -> Hash32 {
+        todo!()
+    }
+
+    pub fn as_hash_20(&self) -> Hash20 {
+        todo!()
+    }
+}
+
+pub struct Proposal {
+    inner: InnerProposal,
+    sig: Bytes,
+    id: ProposalId
+}
+
+pub struct InnerProposal {
+    ballot: Ballot,
+    tx_ids: Vec<TxId>,
+    mesh_hash: Hash32
+}
+
+
+impl Proposal {
+    pub fn init(&mut self) -> ProposalResult<Proposal> {
+        todo!()
+    }
+
+    pub fn id(&self) -> ProposalId {
+        todo!()
+    }
+
+    pub fn set_id(&mut self, id: impl Into<ProposalId>) {
+
+    }
+
+
+}
+
