94af5af171aa35d9602624d9a2c385931714410a
tannr <tannr@hey.com>
2022-12-06 09:12:36 -0800

    basic git actions

--- types/tests/proposal.rs
+++ types/tests/proposal.rs
@@ -1,13 +1,21 @@
 use spacemesh_types::proposal::*;
 
 #[test]
-fn test_proposal_id_size() {}
+fn test_proposal_id_size() {
+
+}
 
 #[test]
-fn test_proposal_init() {}
+fn test_proposal_init() {
+
+}
 
 #[test]
-fn test_proposal_init_bad_signature() {}
+fn test_proposal_init_bad_signature() {
+
+}
 
 #[test]
-fn test_proposal_init_inconsistent_ballot() {}
+fn test_proposal_init_inconsistent_ballot() {
+    
+}
